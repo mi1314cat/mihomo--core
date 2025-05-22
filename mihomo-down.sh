59ea1b197506dd5bdc306e0b017b5bcb24dd5cc1
mi1314cat <135564322+mi1314cat@users.noreply.github.com>
2025-05-22 16:12:12 +0800

    Update mihomo-down.sh

--- mihomo-down.sh
+++ mihomo-down.sh
@@ -91,14 +91,19 @@ else
 
     cat <<EOF | sudo tee "$SERVICE_FILE" > /dev/null
 [Unit]
-Description=Mihomo Service
-After=network.target
+Description=mihomo Daemon, Another Clash Kernel.
+After=network.target NetworkManager.service systemd-networkd.service iwd.service
 
 [Service]
+Type=simple
+LimitNPROC=500
+LimitNOFILE=1000000
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_RAW CAP_NET_BIND_SERVICE CAP_SYS_TIME CAP_SYS_PTRACE CAP_DAC_READ_SEARCH CAP_DAC_OVERRIDE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_RAW CAP_NET_BIND_SERVICE CAP_SYS_TIME CAP_SYS_PTRACE CAP_DAC_READ_SEARCH CAP_DAC_OVERRIDE
+Restart=always
+ExecStartPre=/usr/bin/sleep 1s
 ExecStart=$INSTALL_DIR/mihomo -d $INSTALL_DIR
-Restart=on-failure
-User=root
-LimitNOFILE=65535
+ExecReload=/bin/kill -HUP $MAINPID
 
 [Install]
 WantedBy=multi-user.target
