b020952e2d5274d83fe8e42895131c566514fce4
mi1314cat <135564322+mi1314cat@users.noreply.github.com>
2025-05-24 21:21:28 +0800

    Update mihomo.sh

--- mihomo.sh
+++ mihomo.sh
@@ -73,7 +73,8 @@ openssl req -x509 -nodes -newkey ec:<(openssl ecparam -name prime256v1) \
     -subj "/CN=bing.com" -days 36500 && \
 
 
-
+mv $INSTALL_DIR/server.crt $INSTALL_DIRs/
+mv $INSTALL_DIR/server.key $INSTALL_DIRs/
 
 
 # 定义函数，返回随机选择的域名
@@ -198,8 +199,8 @@ listeners:
   listen: "::"
   users:
     username1: $hy_password
-  certificate: $INSTALL_DIR/server.crt
-  private-key: $INSTALL_DIR/server.key
+  certificate: $INSTALL_DIRs/server.crt
+  private-key: $INSTALL_DIRs/server.key
   padding-scheme: |
    stop=8
    0=30-30
@@ -222,8 +223,8 @@ listeners:
   masquerade: ""
   alpn:
   - h3
-  certificate: $INSTALL_DIR/server.crt
-  private-key: $INSTALL_DIR/server.key
+  certificate: $INSTALL_DIRs/server.crt
+  private-key: $INSTALL_DIRs/server.key
   
 - name: reality
   type: vless
@@ -249,8 +250,8 @@ listeners:
   users:
     uuid: $UUID
     password: $hy_password
-  certificate: $INSTALL_DIR/server.crt
-  private-key: $INSTALL_DIR/server.key
+  certificate: $INSTALL_DIRs/server.crt
+  private-key: $INSTALL_DIRs/server.key
   congestion-controller: bbr
   max-idle-time: 15000
   authentication-timeout: 1000
