b0979822318131d218fc02c650d31c6e5ec358de
mi1314cat <135564322+mi1314cat@users.noreply.github.com>
2025-05-25 21:06:07 +0800

    Update alpine-mihomo.sh

--- alpine-mihomo.sh
+++ alpine-mihomo.sh
@@ -364,7 +364,7 @@ cat << EOF > $INSTALL_DIR/clash-meta.yaml
     tls: false
     network: ws
     ws-opts:
-      path: ${WS_PATH1}
+      path: ${WS_PATH}
       headers: {}
 
   - name: anytls
@@ -401,7 +401,7 @@ cat << EOF > $INSTALL_DIR/clash-meta.yaml
     tls: true
     network: ws
     ws-opts:
-      path: ${WS_PATH1}
+      path: ${WS_PATH}
       headers:
         Host: $DOMAIN_LOWER
     servername: $DOMAIN_LOWER
@@ -418,7 +418,7 @@ cat << EOF > $INSTALL_DIR/clash-meta.yaml
     ws-opts:
       headers:
         Host: $DOMAIN_LOWER
-      path: ${WS_PATH}
+      path: ${WS_PATH1}
     servername: $DOMAIN_LOWER
  
 EOF
